027d8bc86966d8c25378710d67d6f3aa82c4f89c
Esteban Rojas Huerta <esteban_moreeste@hotmail.com>
2026-04-22 00:16:56 -0600

    Creación de método de test para suma en ms test

---
+++ LibreriaVaxiMSTest/OperacionMSTest.cs
@@ -0,0 +1,23 @@
+﻿using LibreriaVaxi;
+
+namespace LibreriaVaxiMSTest
+{
+    [TestClass]
+    public class OperacionMSTest
+    {
+        [TestMethod]
+        public void SumarNumeros_InputDosNumeros_GetValorCorrecto()
+        {
+            // Arrange
+            var op = new Operacion();
+            int numero1 = 50;
+            int numero2 = 69;
+
+            // Act
+            int resultado = op.SumarNumeros(numero1, numero2);
+
+            // Assert
+            Assert.AreEqual(119, resultado);
+        }
+    }
+}
