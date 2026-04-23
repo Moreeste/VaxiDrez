ff98310df9bc1bfce1b28aa5cdd4565ba6a95628
Esteban Rojas Huerta <esteban_moreeste@hotmail.com>
2026-04-22 23:22:03 -0600

    Creación de proyecto NUnit test

---
+++ LibreriaVaxiNUnitTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace LibreriaVaxiNUnitTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
