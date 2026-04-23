b60925a82e5618eea584e3298c79fbdaa58a4921
Esteban Rojas Huerta <esteban_moreeste@hotmail.com>
2026-04-22 23:29:04 -0600

    Creación de método de test para sumar en n unit

---
+++ LibreriaVaxiNUnitTest/OperacionNUnitTest.cs
@@ -0,0 +1,21 @@
+﻿namespace LibreriaVaxi
+{
+    [TestFixture]
+    public class OperacionNUnitTest
+    {
+        [Test]
+        public void SumarNumeros_InputDosNumeros_GetValorCorrecto()
+        {
+            // Arrange
+            var op = new Operacion();
+            int numero1 = 50;
+            int numero2 = 69;
+
+            // Act
+            int resultado = op.SumarNumeros(numero1, numero2);
+
+            // Assert
+            Assert.That(resultado, Is.EqualTo(119));
+        }
+    }
+}
