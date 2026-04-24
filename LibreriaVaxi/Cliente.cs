e20696e9dfa851d08eb5efca865734883a2d1faa
Esteban Rojas Huerta <esteban_moreeste@hotmail.com>
2026-04-24 00:13:05 -0600

    Test para cadena de texto

---
+++ LibreriaVaxi/Cliente.cs
@@ -0,0 +1,10 @@
+﻿namespace LibreriaVaxi
+{
+    public class Cliente
+    {
+        public string CrearNombreCompleto(string nombre, string apellido)
+        {
+            return $"{nombre} {apellido}";
+        }
+    }
+}
---
+++ LibreriaVaxiNUnitTest/ClienteNUnitTest.cs
@@ -0,0 +1,19 @@
+﻿namespace LibreriaVaxi
+{
+    [TestFixture]
+    public class ClienteNUnitTest
+    {
+        [Test]
+        public void CrearNombreCompleto_InputNombreApellido_ReturnNombreCompleto()
+        {
+            //Arrange
+            Cliente cliente = new Cliente();
+
+            //Act
+            string nombreCompleto = cliente.CrearNombreCompleto("Esteban", "Rojas");
+
+            //Assert
+            Assert.That(nombreCompleto, Is.EqualTo("Esteban Rojas"));
+        }
+    }
+}
