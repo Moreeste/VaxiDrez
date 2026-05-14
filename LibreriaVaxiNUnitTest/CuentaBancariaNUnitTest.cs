2c823dcfc1f5155a4e7c52f378178343bdb5df6f
Esteban Rojas Huerta <esteban_moreeste@hotmail.com>
2026-05-14 00:04:11 -0600

    mock con valor default

--- LibreriaVaxiNUnitTest/CuentaBancariaNUnitTest.cs
+++ LibreriaVaxiNUnitTest/CuentaBancariaNUnitTest.cs
@@ -35,5 +35,38 @@ namespace LibreriaVaxi
             Assert.That(result, Is.True);
             Assert.That(cuentaBancaria.GetBalance(), Is.EqualTo(100));
         }
+
+        [Test]
+        [TestCase(200, 100)]
+        [TestCase(200, 150)]
+        public void Retiro_Retiro100ConBalance200_ReturnsTrue(int balance, int retito)
+        {
+            var loggerMock = new Mock<ILoggerGeneral>();
+            loggerMock.Setup(x => x.LogDatabase(It.IsAny<string>())).Returns(true);
+            loggerMock.Setup(x => x.LogBalanceDespuesRetiro(It.Is<int>(x => x > 0))).Returns(true);
+
+            var cuentaBancaria = new CuentaBancaria(loggerMock.Object);
+            cuentaBancaria.Deposito(balance);
+
+            var result = cuentaBancaria.Retiro(retito);
+
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        [TestCase(200, 300)]
+        public void Retiro_Retiro300ConBalance200_ReturnsFalse(int balance, int retito)
+        {
+            var loggerMock = new Mock<ILoggerGeneral>();
+            //loggerMock.Setup(x => x.LogBalanceDespuesRetiro(It.Is<int>(x => x < 0))).Returns(false);
+            loggerMock.Setup(x => x.LogBalanceDespuesRetiro(It.IsInRange<int>(int.MaxValue, -1, Moq.Range.Inclusive))).Returns(false);
+
+            var cuentaBancaria = new CuentaBancaria(loggerMock.Object);
+            cuentaBancaria.Deposito(balance);
+
+            var result = cuentaBancaria.Retiro(retito);
+
+            Assert.That(result, Is.False);
+        }
     }
 }
