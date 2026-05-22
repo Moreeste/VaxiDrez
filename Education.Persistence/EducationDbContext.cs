b523d7b4bbabbf621a917b49e3ebab294b0b4b66
Esteban Rojas Huerta <esteban_moreeste@hotmail.com>
2026-05-22 00:27:08 -0600

    Se agrega persistencia

---
+++ Education.Persistence/EducationDbContext.cs
@@ -0,0 +1,56 @@
+﻿using Education.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Education.Persistence
+{
+    public class EducationDbContext : DbContext
+    {
+        public EducationDbContext(DbContextOptions<EducationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Curso> Cursos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Curso>().Property(x => x.Precio).HasPrecision(14, 2);
+
+            modelBuilder.Entity<Curso>().HasData(
+                new Curso
+                {
+                    CursoId = Guid.NewGuid(),
+                    Titulo = "Curso de C#",
+                    Descripcion = "Aprende los fundamentos de C# y desarrollo de aplicaciones.",
+                    FechaCreacion = DateTime.Now,
+                    FechaPublicacion = DateTime.Now.AddDays(30),
+                    Precio = 56
+                }
+             );
+
+            modelBuilder.Entity<Curso>().HasData(
+                new Curso
+                {
+                    CursoId = Guid.NewGuid(),
+                    Titulo = "Curso de Java",
+                    Descripcion = "Aprende los fundamentos de java y desarrollo de aplicaciones.",
+                    FechaCreacion = DateTime.Now,
+                    FechaPublicacion = DateTime.Now.AddDays(30),
+                    Precio = 57
+                }
+             );
+
+            modelBuilder.Entity<Curso>().HasData(
+                new Curso
+                {
+                    CursoId = Guid.NewGuid(),
+                    Titulo = "Curso de Python",
+                    Descripcion = "Aprende los fundamentos de Python y desarrollo de aplicaciones.",
+                    FechaCreacion = DateTime.Now,
+                    FechaPublicacion = DateTime.Now.AddDays(30),
+                    Precio = 58
+                }
+             );
+        }
+    }
+}
