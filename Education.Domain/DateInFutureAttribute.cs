bbfb8ab0098315418e6e50f810c9fb822364543d
Esteban Rojas Huerta <esteban_moreeste@hotmail.com>
2026-05-22 00:17:51 -0600

    Creación de entidad curso

---
+++ Education.Domain/DateInFutureAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Education.Domain
+{
+    public class DateInFutureAttribute : ValidationAttribute
+    {
+        private readonly Func<DateTime> _dateNowProvider;
+
+        public DateInFutureAttribute() : this(() => DateTime.Now)
+        {
+            
+        }
+
+        public DateInFutureAttribute(Func<DateTime> dateNowProvider)
+        {
+            _dateNowProvider = dateNowProvider;
+            ErrorMessage = "La fecha debe ser en el futuro.";
+        }
+
+        public override bool IsValid(object value)
+        {
+            bool isValid = false;
+
+            if (value is DateTime dateTime)
+            {
+                isValid = dateTime > _dateNowProvider();
+            }
+
+            return isValid;
+        }
+    }
+}
