499f3d672a2ac04502d7ab0a05694d8ec8fb32bc
Esteban Rojas Huerta <esteban_moreeste@hotmail.com>
2026-05-26 00:05:17 -0600

    cqrs para consultar curso por id

---
+++ Education.Application/Cursos/GetCursoByIdQuery.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Education.Application.Dto;
+using Education.Domain;
+using Education.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Education.Application.Cursos
+{
+    public class GetCursoByIdQueryRequest : IRequest<CursoDto>
+    {
+        public Guid Id { get; set; }
+    }
+
+    public class GetCursoByIdQueryHandler(EducationDbContext context, IMapper mapper) : IRequestHandler<GetCursoByIdQueryRequest, CursoDto>
+    {
+        public async Task<CursoDto> Handle(GetCursoByIdQueryRequest request, CancellationToken cancellationToken)
+        {
+            var curso = await context.Cursos.FirstOrDefaultAsync(x => x.CursoId == request.Id);
+            var cursoDto = mapper.Map<Curso, CursoDto>(curso);
+            return cursoDto;
+        }
+    }
+}
