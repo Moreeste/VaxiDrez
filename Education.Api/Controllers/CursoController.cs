d1c0b79f9228491d778d4536fea2e39d37338b6d
Esteban Rojas Huerta <esteban_moreeste@hotmail.com>
2026-06-02 00:11:43 -0600

    Creación de apis de curso

---
+++ Education.Api/Controllers/CursoController.cs
@@ -0,0 +1,24 @@
+﻿using Education.Application.Cursos;
+using Education.Application.Dto;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Education.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CursoController(IMediator mediator) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<CursoDto>>> Get()
+        {
+            return await mediator.Send(new GetCursoQueryRequest());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Unit>> Post([FromBody] CreateCursoCommandRequest request)
+        {
+            return await mediator.Send(request);
+        }
+    }
+}
--- Education.Api/Program.cs
+++ Education.Api/Program.cs
@@ -1,11 +1,32 @@
+using Education.Application.Cursos;
+using Education.Persistence;
+using FluentValidation.AspNetCore;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<EducationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+ );
 
-builder.Services.AddControllers();
+builder.Services.AddMediatR(typeof(GetCursoByIdQueryHandler).Assembly);
+
+builder.Services.AddControllers().AddFluentValidation(x =>
+{
+    x.RegisterValidatorsFromAssemblyContaining<CreateCursoCommandRequest>();
+});
+
+builder.Services.AddAutoMapper(typeof(GetCursoByIdQueryHandler).Assembly);
+
+builder.Services.AddCors(x => x.AddPolicy("corsApp", builder =>
+{
+    builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+}));
 
 var app = builder.Build();
-
+app.UseCors("corsApp");
 // Configure the HTTP request pipeline.
 
 app.UseAuthorization();
