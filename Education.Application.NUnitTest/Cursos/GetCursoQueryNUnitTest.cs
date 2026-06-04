23fd68d010c03cfc20aa8788bba2e2da61ea3710
Esteban Rojas Huerta <esteban_moreeste@hotmail.com>
2026-06-03 23:39:30 -0600

    Preparación de métodos de test para el handler cqrs

---
+++ Education.Application.NUnitTest/Cursos/GetCursoQueryNUnitTest.cs
@@ -0,0 +1,41 @@
+﻿using AutoFixture;
+using AutoMapper;
+using Education.Application.Helper;
+using Education.Domain;
+using Education.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Education.Application.Cursos
+{
+    [TestFixture]
+    public class GetCursoQueryNUnitTest
+    {
+        private GetCursoQueryHandler _handler;
+
+        [SetUp]
+        public void Setup()
+        {
+            var fixture = new Fixture();
+            var cursoRecords = fixture.CreateMany<Curso>().ToList();
+            cursoRecords.Add(fixture.Build<Curso>().With(x => x.CursoId, Guid.Empty).Create());
+
+            var options = new DbContextOptionsBuilder<EducationDbContext>().UseInMemoryDatabase(databaseName: $"EducationDb-{Guid.NewGuid()}").Options;
+            var educationDbContextFake = new EducationDbContext(options);
+            educationDbContextFake.Cursos.AddRange(cursoRecords);
+
+            var mapConfig = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new MappingTest());
+            });
+            var mapper = mapConfig.CreateMapper();
+
+            _handler = new GetCursoQueryHandler(educationDbContextFake, mapper);
+        }
+
+        [Test]
+        public void GetCursoQueryHandler_ConsultaCursos_ReturnsTrue()
+        {
+
+        }
+    }
+}
---
+++ Education.Application.NUnitTest/Helper/MappingTest.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Education.Application.Dto;
+using Education.Domain;
+
+namespace Education.Application.Helper
+{
+    public class MappingTest : Profile
+    {
+        public MappingTest()
+        {
+            CreateMap<Curso, CursoDto>();
+        }
+    }
+}
