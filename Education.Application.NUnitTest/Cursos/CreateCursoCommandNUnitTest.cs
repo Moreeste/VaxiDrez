fcb3aa571d0d369cf3fc6ce664db060fac2ae05b
Esteban Rojas Huerta <esteban_moreeste@hotmail.com>
2026-06-04 00:00:09 -0600

    Creación de dest para CreateCursoCommandHandler

---
+++ Education.Application.NUnitTest/Cursos/CreateCursoCommandNUnitTest.cs
@@ -0,0 +1,39 @@
+﻿using AutoFixture;
+using AutoMapper;
+using Education.Domain;
+using Education.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Education.Application.Cursos
+{
+    public class CreateCursoCommandNUnitTest
+    {
+        private CreateCursoCommandHandler _handler;
+
+        [SetUp]
+        public void Setup()
+        {
+            var options = new DbContextOptionsBuilder<EducationDbContext>().UseInMemoryDatabase(databaseName: $"EducationDb-{Guid.NewGuid()}").Options;
+            var educationDbContextFake = new EducationDbContext(options);
+
+            _handler = new CreateCursoCommandHandler(educationDbContextFake);
+        }
+
+        [Test]
+        public async Task CreateCursoCommandHandler_InputCurso_ReturnsNumber()
+        {
+            var command = new CreateCursoCommandRequest
+            {
+                Titulo = "Curso de Prueba",
+                Descripcion = "Descripción del curso de prueba",
+                FechaPublicacion = DateTime.UtcNow,
+                Precio = 99.99m
+            };
+
+            var resultado = await _handler.Handle(command, CancellationToken.None);
+
+            Assert.That(resultado, Is.EqualTo(Unit.Value));
+        }
+    }
+}
